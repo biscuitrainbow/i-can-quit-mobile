01be523b8aee4727358f59ac6b4a3aeba3600d2f
Natthapon Sricort <natthaponsricort@gmail.com>
2019-09-07 20:20:01 +0700

    Init

---
+++ android/app/src/main/kotlin/i/can/quit/camt/cmu/ac/th/i_can_quit/MainActivity.kt
@@ -0,0 +1,13 @@
+package i.can.quit.camt.cmu.ac.th.i_can_quit
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
