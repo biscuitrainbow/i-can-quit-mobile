6f3760b0d247af743aff6ade4e1a199d6a1837ae
Natthapon Sricort <natthaponsricort@gmail.com>
2019-12-05 23:38:18 +0700

    Remove google map package.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/biscuitrainbow/code/sdk/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/biscuitrainbow/code/i-can-quit-mobile"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\Natth\fvm\versions\stable"
+export "FLUTTER_APPLICATION_PATH=D:\code\i-can-quit\mobile\i_can_quit"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/biscuitrainbow/code/sdk/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Natth\fvm\versions\stable\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=3"
--- lib/ui/screen/smoking_entry/smoking_entry_cluster_map.dart
+++ lib/ui/screen/smoking_entry/smoking_entry_cluster_map.dart
@@ -2,9 +2,9 @@ import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/plugin_api.dart';
 import 'package:meta/meta.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class SmokingEntryClusterMap extends StatefulWidget {
   @override
@@ -12,7 +12,7 @@ class SmokingEntryClusterMap extends StatefulWidget {
 }
 
 class _SmokingEntryClusterMapState extends State<SmokingEntryClusterMap> {
-  List<Marker> _marker = [];
+  // List<Marker> _marker = [];
 
   @override
   void initState() {
@@ -20,22 +20,22 @@ class _SmokingEntryClusterMapState extends State<SmokingEntryClusterMap> {
   }
 
   void _createMarkser() async {
-    final List<LatLng> locations = [
-      LatLng(18.7970639, 99.0092428),
-      LatLng(18.797267, 99.0265377),
-    ];
+    // final List<LatLng> locations = [
+    //   LatLng(18.7970639, 99.0092428),
+    //   LatLng(18.797267, 99.0265377),
+    // ];
 
     setState(() {
-      locations.forEach((location) async {
-        final icon = BitmapDescriptor.fromBytes(await getBytesFromCanvas(20));
-        final marker = Marker(
-          position: location,
-          icon: icon,
-          markerId: null,
-        );
+      // locations.forEach((location) async {
+      //   final icon = BitmapDescriptor.fromBytes(await getBytesFromCanvas(20));
+      //   final marker = Marker(
+      //     position: location,
+      //     icon: icon,
+      //     markerId: null,
+      //   );
 
-        _marker.add(marker);
-      });
+      //   _marker.add(marker);
+      // });
     });
   }
 
@@ -44,41 +44,39 @@ class _SmokingEntryClusterMapState extends State<SmokingEntryClusterMap> {
     _createMarkser();
 
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(18.7970639, 99.0092428),
-          zoom: 15,
+      body: FlutterMap(
+        options: new MapOptions(
+          // center: new LatLng(51.5, -0.09),
+          zoom: 13.0,
+          plugins: [
+            MyCustomPlugin(),
+          ],
         ),
-        markers: _marker.toSet(),
+        layers: [
+          new TileLayerOptions(
+            urlTemplate: "https://api.tiles.mapbox.com/v4/"
+                "{id}/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiYmlzY3VpdHJhaW5ib3ciLCJhIjoiY2prdHNxN3YyMDlobDN2cGJ0aGdmOHk3YyJ9.Bf6KFoZAsx6rGa7h5Cl_og",
+            additionalOptions: {
+              'accessToken': 'pk.eyJ1IjoiYmlzY3VpdHJhaW5ib3ciLCJhIjoiY2prdHNxN3YyMDlobDN2cGJ0aGdmOHk3YyJ9.Bf6KFoZAsx6rGa7h5Cl_og',
+              'id': 'mapbox.streets',
+            },
+          ),
+          MyCustomPluginOptions(text: "I'm a plugin!"),
+          new MarkerLayerOptions(
+            markers: [
+              new Marker(
+                width: 80.0,
+                height: 80.0,
+                // point: new LatLng(51.5, -0.09),
+                builder: (ctx) => new Container(
+                  child: new FlutterLogo(),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
-  }
-
-  Future<Uint8List> getBytesFromCanvas(int nodeSize) async {
-    final PictureRecorder pictureRecorder = PictureRecorder();
-    MaterialColor color = _getColor(nodeSize);
-    final Canvas canvas = Canvas(pictureRecorder);
-    final Paint paint1 = Paint()..color = color[400];
-    final Paint paint2 = Paint()..color = color[300];
-    final Paint paint3 = Paint()..color = color[100];
-    final int size = 120;
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint3);
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.4, paint2);
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 3.3, paint1);
-    TextPainter painter = TextPainter(textDirection: TextDirection.ltr);
-    painter.text = TextSpan(
-      text: nodeSize.toString(),
-      style: TextStyle(fontSize: size / 4, color: Colors.black, fontWeight: FontWeight.bold),
-    );
-    painter.layout();
-    painter.paint(
-      canvas,
-      Offset(size / 2 - painter.width / 2, size / 2 - painter.height / 2),
-    );
-
-    final img = await pictureRecorder.endRecording().toImage(size, size);
-    final data = await img.toByteData(format: ImageByteFormat.png);
-    return data.buffer.asUint8List();
   }
 
   MaterialColor _getColor(int size) {
@@ -103,5 +101,35 @@ class _SmokingEntryClusterMapState extends State<SmokingEntryClusterMap> {
     }
 
     return color;
+  }
+}
+
+class MyCustomPluginOptions extends LayerOptions {
+  final String text;
+  MyCustomPluginOptions({this.text = ''});
+}
+
+class MyCustomPlugin implements MapPlugin {
+  @override
+  Widget createLayer(LayerOptions options, MapState mapState, Stream<Null> stream) {
+    if (options is MyCustomPluginOptions) {
+      var style = TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 24.0,
+        color: Colors.red,
+      );
+
+      return Text(
+        options.text,
+        style: style,
+      );
+    }
+    throw Exception('Unknown options type for MyCustom'
+        'plugin: $options');
+  }
+
+  @override
+  bool supportsLayer(LayerOptions options) {
+    return options is MyCustomPluginOptions;
   }
 }
