6a8bb71378cf37e14d87723a789910132ce330ad
Natthapon Sricort <natthaponsricort@gmail.com>
2019-12-12 22:51:02 +0700

    Remove "flutter_map" package from application.

--- lib/ui/screen/smoking_entry/smoking_entry_cluster_map.dart
+++ lib/ui/screen/smoking_entry/smoking_entry_cluster_map.dart
@@ -2,9 +2,7 @@ import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_map/plugin_api.dart';
 import 'package:meta/meta.dart';
-import 'package:flutter_map/flutter_map.dart';
 
 class SmokingEntryClusterMap extends StatefulWidget {
   @override
@@ -44,92 +42,8 @@ class _SmokingEntryClusterMapState extends State<SmokingEntryClusterMap> {
     _createMarkser();
 
     return Scaffold(
-      body: FlutterMap(
-        options: new MapOptions(
-          // center: new LatLng(51.5, -0.09),
-          zoom: 13.0,
-          plugins: [
-            MyCustomPlugin(),
-          ],
-        ),
-        layers: [
-          new TileLayerOptions(
-            urlTemplate: "https://api.tiles.mapbox.com/v4/"
-                "{id}/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiYmlzY3VpdHJhaW5ib3ciLCJhIjoiY2prdHNxN3YyMDlobDN2cGJ0aGdmOHk3YyJ9.Bf6KFoZAsx6rGa7h5Cl_og",
-            additionalOptions: {
-              'accessToken': 'pk.eyJ1IjoiYmlzY3VpdHJhaW5ib3ciLCJhIjoiY2prdHNxN3YyMDlobDN2cGJ0aGdmOHk3YyJ9.Bf6KFoZAsx6rGa7h5Cl_og',
-              'id': 'mapbox.streets',
-            },
-          ),
-          MyCustomPluginOptions(text: "I'm a plugin!"),
-          new MarkerLayerOptions(
-            markers: [
-              new Marker(
-                width: 80.0,
-                height: 80.0,
-                // point: new LatLng(51.5, -0.09),
-                builder: (ctx) => new Container(
-                  child: new FlutterLogo(),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+    
     );
   }
-
-  MaterialColor _getColor(int size) {
-    MaterialColor color;
-
-    if (size < 10) {
-      color = Colors.grey;
-    } else if (size < 25) {
-      color = Colors.blue;
-    } else if (size < 25) {
-      color = Colors.blue;
-    } else if (size < 50) {
-      color = Colors.green;
-    } else if (size < 100) {
-      color = Colors.yellow;
-    } else if (size < 500) {
-      color = Colors.orange;
-    } else if (size < 1000) {
-      color = Colors.red;
-    } else {
-      color = Colors.pink;
-    }
-
-    return color;
-  }
 }
 
-class MyCustomPluginOptions extends LayerOptions {
-  final String text;
-  MyCustomPluginOptions({this.text = ''});
-}
-
-class MyCustomPlugin implements MapPlugin {
-  @override
-  Widget createLayer(LayerOptions options, MapState mapState, Stream<Null> stream) {
-    if (options is MyCustomPluginOptions) {
-      var style = TextStyle(
-        fontWeight: FontWeight.bold,
-        fontSize: 24.0,
-        color: Colors.red,
-      );
-
-      return Text(
-        options.text,
-        style: style,
-      );
-    }
-    throw Exception('Unknown options type for MyCustom'
-        'plugin: $options');
-  }
-
-  @override
-  bool supportsLayer(LayerOptions options) {
-    return options is MyCustomPluginOptions;
-  }
-}
